ecde5ef13942b2902abc971b0ccd03d2611f84d6
resinner <revengingsinner@gmail.com>
2022-11-04 17:04:14 +0200

    Task1 complite

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const body = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+stopBtn.setAttribute('disabled', true);
+
+let interval = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener('click', () => {
+    interval = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+        startBtn.setAttribute('disabled', true);
+         stopBtn.removeAttribute('disabled');
+    }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+    startBtn.removeAttribute('disabled');
+    stopBtn.setAttribute('disabled', true);
+    clearInterval(interval);
+});
